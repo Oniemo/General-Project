ace489c06d402b0cfef28cc2d4eadfa98601f094
User <User@DENIS>
2026-09-11 20:02:28 +0500

    N/A

---
+++ test2/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using 
+using test2.Models;
+
+namespace test2.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+            "Host=localhost;" +
+            "Port=5432;" +
+            "Database=Product;" +
+            "Username=postgres;" +
+            "Password=ROOT");
+        }
+    }
+}
